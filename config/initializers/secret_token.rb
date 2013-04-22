d32217dd30eac8a67106cc6f9f4a20a22a087345
maahd <maahdsh@gmail.com>
2013-04-22 15:46:15 +0300

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandBlitz::Application.config.secret_token = '31bc1ff4a18c2da6e231eddc030c96007218fb0eeb3ca9071723d1d7500f5e1f1255189a4430c2d95cb2e06adc5a93dc2d6041fed0ce8b57cdfee07b539eca52'
