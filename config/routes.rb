f10d9559dcb02b0ade6cd1a8b0f05e38e228b2e5
maahd <maahdsh@gmail.com>
2013-04-22 17:03:28 +0300

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BandBlitz::Application.routes.draw do
+  get "static/pages"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
